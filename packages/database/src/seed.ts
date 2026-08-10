8492edd44909f6e2fe7416c9caacc5454dbb7155
Alif Rahman <alif.rahman.c@gmail.com>
2026-08-10 13:51:29 +0600

    database seeder

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { hash } from "argon2";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
 
 const prisma = new PrismaClient();
 
