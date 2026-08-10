8c8b77c27cb58db2c378f4ca314727e1325722f6
Alif Rahman <alif.rahman.c@gmail.com>
2026-08-11 00:28:44 +0600

    prisma test phase completed

--- test-prisma-resolve.cjs
+++
@@ -1,8 +0,0 @@
-try {
-  const pkg = require.resolve('prisma/package.json');
-  console.log('Found prisma package.json at:', pkg);
-  const prismaPath = require.resolve('prisma');
-  console.log('Found prisma at:', prismaPath);
-} catch (e) {
-  console.error('Cannot find prisma:', e.message);
-}
